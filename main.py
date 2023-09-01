d4218cada20edeae11f37bc02bb3a9891fcb0bd6
HamCam203 <camarahamoudia@gmail.com>
2023-09-01 15:02:19 +0200

    ajoutdetruc

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ TOKEN_TYPES = {
     'IDENTIFIER': 'IDENTIFIER',
 }
 NODES_TYPES = {
-    'NODE_CLOSE_PAREN': 'NODE_CLOSE_PAREN',
-    'NODE_OPEN_PAREN': 'NODE_OPEN_PAREN',
+    'NODE_IDENTIFIER': 'NODE_IDENTIFIER',
+    'NODE_CONSTANT': 'NODE_CONSTANT',
+    'NODE_MINUS_UNARY': 'NODE_MINUS_UNARY',
+    'NODE_MINUS_BINARY': 'NODE_MINUS_BINARY',
+    'NODE_NOT': 'NODE_NOT',
+    'NODE_PLUS': 'NODE_PLUS',
+
 }
 MOTS_CLES = {
     'int': 'int',
@@ -40,6 +45,22 @@ MOTS_CLES = {
     'long': 'long',
     'short': 'short',
     'signed': 'signed',
+}
+
+class ValOpe:
+    def __init__(self, nde, priority, AaD):
+        self.nde = nde
+        self.priority =priority
+        self.AaD = AaD
+
+# Table des Opérateurs avec les priorités
+OPERATORS = {
+    TOKEN_TYPES['PLUS']: ValOpe('PLUS',6,0),
+    TOKEN_TYPES['MINUS']: ValOpe('MINUS',6,0),
+    TOKEN_TYPES['MUL']: ValOpe('MUL',7,0),
+    TOKEN_TYPES['DIV']: ValOpe('DIV',7,0),
+    TOKEN_TYPES['MOD']: ValOpe('MOD',7,0),
+
 }
 
 class Token:
@@ -76,11 +97,6 @@ class Node:
         print("Le noeud est de type : ", self.type, " et sa valeur est : ", self.value)
         for child in self.children:
             child.affiche()
-
-
-
-    
-
 
 tokenG = Token('test',0) #token courant
 last = Token('test',1) #token précédent 
@@ -138,14 +154,71 @@ def next(chaine):
         return Token("EOF",None)
 
 def check(self, token_type):
-    return tokenG.type == TOKEN_TYPES[token_type]
+    if(tokenG.type == TOKEN_TYPES[token_type]):
+        self.next()
+        return True
+    else :
+        return False
 
 def accept(self, token_type):
     if not self.check(token_type):
-        self.next()
-        return True
-    return False
-    
+        raise Exception("Le token est invalid")
+
+def Atome():
+    if(check(TOKEN_TYPES['CONSTANT'])) :
+        return Node(NODES_TYPES["NODE_CONSTANT"],tokenG.value)
+    elif(check(TOKEN_TYPES['IDENTIFIER'])):
+        return Node(NODES_TYPES["NODE_IDENTIFIER"],tokenG.value)
+    elif(check(TOKEN_TYPES['OPEN_PAREN'])):
+        N = E()
+        accept(TOKEN_TYPES['CLOSE_PAREN'])
+        return N
+    else :
+        raise Exception("Le token est invalid")
+
+def prefix():
+    if(check(TOKEN_TYPES['TOKEN_MINUS'])) :
+        N = prefix()
+        return Node(NODES_TYPES["NODE_MINUS_UNAIRY"],tokenG.value)
+
+    elif(check(TOKEN_TYPES['TOKEN_NOT'])) :
+        N = prefix()
+        return Node(NODES_TYPES["NODE_NOT"],tokenG.value)
+
+    elif(check(TOKEN_TYPES['TOKEN_PLUS'])) :
+        N = prefix()
+        return N
+
+    else :
+        N = Atome()
+        return N
+
+def Expression(Prio_min): #Parseur de Brat
+    N = P()
+    while(OPERATORS[tokenG.type] != None):
+        Op = OPERATORS[tokenG.type]
+        if(Op.priority <= Prio_min) :
+            break
+        else:
+            next()
+            M = E(Op.priority - Op.AaD)
+            N = Node(Op.nde,N,M)
+    return N
+        
+def  Genecode(N):
+    if N.type == 'NODE_CONSTANT' :
+        print("push",N.valeur)
+    elif N.type == 'NODE_NOT' :
+        Gencode(N.children[0])
+        print("not")
+    elif N.type == 'NODE_PLUS':
+        Genecode(N.children[0])
+        Genecode(N.children[1])
+        print("add")
+    elif N.type == 'NODE_MINUS_BINARY':
+        Genecode(N.children[0])
+        Genecode(N.children[1])
+        print("sub")
 
 # Main program
 def main():
